3d771ffac403220c176c122e52c3f916dcfa70e5
hjkim0905 <eax9952@gmail.com>
2023-11-09 00:24:03 +0900

    BoardVO.java bean 작성 완료

---
+++ src/main/java/com/example/bean/BoardVO.java
@@ -0,0 +1,38 @@
+package com.example.bean;
+
+public class BoardVO {
+    private int seq;
+    private String name;
+    private String gender;
+    private String birthday;
+    private String address1;
+    private String address2;
+    private String email;
+    private String telecom;
+    private String phoneNumber;
+    private String isCheckValues;
+    private String introduce;
+
+    public int getSeq()_ { return seq; }
+    public void setSeq(int seq) { this.seq = seq; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+    public String getBirthday() { return birthday; }
+    public void setBirthday(String birthday) { this.birthday = birthday; }
+    public String getAddress1() { return address1; }
+    public void setAddress1(String address1) { this.address1 = address1; }
+    public String getAddress2() { return address2; }
+    public void setAddress2(String address2) { this.address2 = address2; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getTelecom() { return telecom; }
+    public void setTelecom(String telecom) { this.telecom = telecom; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public String getIntroduce() { return introduce; }
+    public void setIntroduce(String introduce) { this.introduce = introduce; }
+    public String getIsCheckValues() { return isCheckValues; }
+    public void setIsCheckValues(String isCheckValues) { this.isCheckValues = isCheckValues; }
+}
